18cd8888e391f7497f9b101fc22ad606dd1ee3f3
tsaikd <tsaikd@gmail.com>
2016-11-23 16:48:18 +0800

    update dependencies
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- docker/build-in-docker.sh
+++ docker/build-in-docker.sh
@@ -10,14 +10,10 @@ projname="gogstash"
 renice 15 $$
 cd "${PD}/.."
 
-gobuilder version -c ">=0.1.3" &>/dev/null || go get -u -v "github.com/tsaikd/gobuilder"
+gobuilder version -c ">=0.1.4" &>/dev/null || go get -u -v "github.com/tsaikd/gobuilder"
+gobuilder --check --test
 
-gobuilder checkerror
-gobuilder checkfmt
-gobuilder checkredundant
-gobuilder restore
-gobuilder get --test --all
-gobuilder build
+go get -v "github.com/stretchr/testify"
 go test -v ./cmd/... ./config/... ./filter/...
 
 if [ "${GITHUB_TOKEN}" ] ; then
