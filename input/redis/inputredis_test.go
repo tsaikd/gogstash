16bef37211431f9e49a0b89802916f0915d07a75
tengattack <tengattack@foxmail.com>
2018-09-21 19:56:47 +0800

    Update input redis test

--- input/redis/inputredis_test.go
+++ input/redis/inputredis_test.go
@@ -2,73 +2,103 @@ package inputredis
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/alicebob/miniredis"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tsaikd/gogstash/config"
 	"github.com/tsaikd/gogstash/config/goglog"
-	"github.com/tsaikd/gogstash/config/logevent"
-	"github.com/tsaikd/gogstash/output/redis"
 )
+
+var s *miniredis.Miniredis
+var timeNow time.Time
 
 func init() {
 	goglog.Logger.SetLevel(logrus.DebugLevel)
 	config.RegistInputHandler(ModuleName, InitHandler)
-	config.RegistOutputHandler(outputredis.ModuleName, outputredis.InitHandler)
 }
 
-func Test_input_redis_module(t *testing.T) {
+func TestMain(m *testing.M) {
+	// initialize redis server
+	s = miniredis.NewMiniRedis()
+	err := s.StartAddr("localhost:6380") // change the port
+	if err != nil {
+		panic(err)
+	}
+	defer s.Close()
+
+	timeNow = time.Now().UTC()
+	ret := m.Run()
+
+	os.Exit(ret)
+}
+
+func Test_input_redis_module_batch(t *testing.T) {
 	assert := assert.New(t)
 	assert.NotNil(assert)
 	require := require.New(t)
 	require.NotNil(require)
 
-	// write test event to redis
+	for i := 0; i < 10; i++ {
+		s.Lpush("gogstash-test", fmt.Sprintf("{\"@timestamp\":\"%s\",\"message\":\"inputredis test message\"}", timeNow.Format(time.RFC3339Nano)))
+	}
+
 	ctx := context.Background()
-	confWrite, err := config.LoadFromYAML([]byte(strings.TrimSpace(`
-debugch: true
-output:
-  - type: redis
-    host:
-      - localhost:6379
-    key: gogstash-test
-    data_type: list
-	`)))
-	require.NoError(err)
-	err = confWrite.Start(ctx)
-	if err != nil {
-		require.True(outputredis.ErrorPingFailed.In(err))
-		t.Skip("skip test input redis module")
-	}
-
-	timestamp := time.Now()
-	confWrite.TestInputEvent(logevent.LogEvent{
-		Timestamp: timestamp,
-		Message:   "inputredis test message",
-	})
-
-	if event, err2 := confWrite.TestGetOutputEvent(300 * time.Millisecond); assert.NoError(err2) {
-		require.Equal(timestamp.UnixNano(), event.Timestamp.UnixNano())
-		require.Equal("inputredis test message", event.Message)
-	}
-
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 	conf, err := config.LoadFromYAML([]byte(strings.TrimSpace(`
 debugch: true
 input:
   - type: redis
-    host: localhost:6379
+    host: localhost:6380
     key: gogstash-test
+    connections: 1
+    batch_count: 10
+	`)))
+	require.NoError(err)
+	require.NoError(conf.Start(ctx))
+
+	time.Sleep(500 * time.Millisecond)
+	for i := 0; i < 10; i++ {
+		if event, err := conf.TestGetOutputEvent(100 * time.Millisecond); assert.NoError(err) {
+			require.Equal(timeNow.UnixNano(), event.Timestamp.UnixNano())
+			require.Equal("inputredis test message", event.Message)
+		}
+	}
+}
+
+func Test_input_redis_module_single(t *testing.T) {
+	assert := assert.New(t)
+	assert.NotNil(assert)
+	require := require.New(t)
+	require.NotNil(require)
+
+	s.Lpush("gogstash-test", fmt.Sprintf("{\"@timestamp\":\"%s\",\"message\":\"inputredis test message\"}", timeNow.Format(time.RFC3339Nano)))
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	conf, err := config.LoadFromYAML([]byte(strings.TrimSpace(`
+debugch: true
+input:
+  - type: redis
+    host: localhost:6380
+    key: gogstash-test
+    batch_count: 1
+    blocking_timeout: 5s
 	`)))
 	require.NoError(err)
 	require.NoError(conf.Start(ctx))
 
 	time.Sleep(500 * time.Millisecond)
 	if event, err := conf.TestGetOutputEvent(100 * time.Millisecond); assert.NoError(err) {
-		require.Equal(timestamp.UnixNano(), event.Timestamp.UnixNano())
+		require.Equal(timeNow.UnixNano(), event.Timestamp.UnixNano())
 		require.Equal("inputredis test message", event.Message)
 	}
 }
