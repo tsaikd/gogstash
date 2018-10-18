3d477f9f348e35b399a4c1dcbb20c9a007bb0d6d
Jeff Buchbinder <jeff@ourexchange.net>
2018-10-18 09:17:18 -0400

    output/elastic: Move to v6 (modern ES version)

--- output/elastic/outputelastic.go
+++ output/elastic/outputelastic.go
@@ -10,7 +10,7 @@ import (
 	"github.com/tsaikd/gogstash/config"
 	"github.com/tsaikd/gogstash/config/goglog"
 	"github.com/tsaikd/gogstash/config/logevent"
-	elastic "gopkg.in/olivere/elastic.v5"
+	elastic "gopkg.in/olivere/elastic.v6"
 )
 
 // ModuleName is the name used in config file
