1a2bed6e0d7d03d65f928c05036ce14aae087413
tsaikd <tsaikd@gmail.com>
2019-01-03 21:28:55 +0900

    fix goglog test case
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- config/goglog/goglog_test.go
+++ config/goglog/goglog_test.go
@@ -3,12 +3,14 @@ package goglog
 import (
 	"bytes"
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tsaikd/KDGoLib/logrusutil"
+	"github.com/tsaikd/KDGoLib/runtimecaller"
 )
 
 func (t *LoggerType) setDebugOutput(stdout io.Writer, stderr io.Writer) {
@@ -62,11 +64,21 @@ func TestLoggerFileLine(t *testing.T) {
 
 	logger.setDebugOutput(stdoutBuffer, stderrBuffer)
 	formatter := &logrusutil.ConsoleLogFormatter{
-		TimestampFormat: timestampFormat,
-		CallerOffset:    5,
+		TimestampFormat:      timestampFormat,
+		CallerOffset:         5,
+		RuntimeCallerFilters: []runtimecaller.Filter{forTestFilterGoglogRuntimeCaller},
 	}
 	logger.setDebugFormatter(formatter)
 
 	logger.Info("Info")
-	require.Contains(stdoutBuffer.String(), "goglog_test.go:70 [info] Info")
+	require.Contains(stdoutBuffer.String(), "goglog_test.go:73 [info] Info")
+}
+
+func forTestFilterGoglogRuntimeCaller(callinfo runtimecaller.CallInfo) (valid bool, stop bool) {
+	if strings.Contains(callinfo.PackageName(), "github.com/tsaikd/gogstash/config/goglog") {
+		if callinfo.FileName() == "goglog_test.go" {
+			return true, false
+		}
+	}
+	return filterGoglogRuntimeCaller(callinfo)
 }
