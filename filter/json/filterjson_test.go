7bd86ef7a3b4abacea5af88a9b90815dc15103e2
Horaci Macias <hmacias@avaya.com>
2019-05-21 00:00:00 +0200

    Allow using any source field. By default it will be 'message'

--- filter/json/filterjson_test.go
+++ filter/json/filterjson_test.go
@@ -58,3 +58,48 @@ filter:
 		require.Equal(expectedEvent, event)
 	}
 }
+
+func Test_filter_json_module_source(t *testing.T) {
+	assert := assert.New(t)
+	assert.NotNil(assert)
+	require := require.New(t)
+	require.NotNil(require)
+
+	ctx := context.Background()
+	conf, err := config.LoadFromYAML([]byte(strings.TrimSpace(`
+debugch: true
+filter:
+  - type: json
+    message: message
+    timestamp: time
+    source: myfield
+    timeformat: "2006-01-02T15:04:05Z"
+	`)))
+	require.NoError(err)
+	require.NoError(conf.Start(ctx))
+
+	timestamp, err := time.Parse("2006-01-02T15:04:05Z", "2016-12-04T09:09:41.193Z")
+	require.NoError(err)
+	fieldvalue := `{ "message": "Test", "host":"Hostname", "time":"2016-12-04T09:09:41.193Z", "tags": [ "foo" ] }`
+
+	expectedEvent := logevent.LogEvent{
+		Timestamp: timestamp,
+		Message:   "Test",
+		Extra: map[string]interface{}{
+			"host":    "Hostname",
+			"myfield": fieldvalue,
+		},
+		Tags: []string{"foo"},
+	}
+
+	conf.TestInputEvent(logevent.LogEvent{
+		Timestamp: time.Now(),
+		Extra: map[string]interface{}{
+			"myfield": fieldvalue,
+		},
+	})
+
+	if event, err := conf.TestGetOutputEvent(300 * time.Millisecond); assert.NoError(err) {
+		require.Equal(expectedEvent, event)
+	}
+}
