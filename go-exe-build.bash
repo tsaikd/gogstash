5d4a2e8ae59912f4892ae0e0b1d65fe6f2cfd2a2
Mayur Virkar <mayurvirkar@gmail.com>
2020-02-04 22:14:59 +0530

    added bin and bin generation script

---
+++ go-exe-build.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+package_name=gogstash
+
+platforms=("linux/amd64" "windows/amd64" "freebsd/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=bin/$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi  
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
+
