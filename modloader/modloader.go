db75cc95c6d9b09b9cf94d8dfd7b6f3d92be3468
Helge Olav Helgesen <1-bitbucket@helge.net>
2021-06-10 22:41:51 +0200

    UDP packet mode

--- modloader/modloader.go
+++ modloader/modloader.go
@@ -11,6 +11,7 @@ import (
 	filtergrok "github.com/tsaikd/gogstash/filter/grok"
 	filterjson "github.com/tsaikd/gogstash/filter/json"
 	filterkv "github.com/tsaikd/gogstash/filter/kv"
+	"github.com/tsaikd/gogstash/filter/kv"
 	filterlookuptable "github.com/tsaikd/gogstash/filter/lookuptable"
 	filtermutate "github.com/tsaikd/gogstash/filter/mutate"
 	filterratelimit "github.com/tsaikd/gogstash/filter/ratelimit"
@@ -75,6 +76,7 @@ func init() {
 	config.RegistFilterHandler(filterurlparam.ModuleName, filterurlparam.InitHandler)
 	config.RegistFilterHandler(filterlookuptable.ModuleName, filterlookuptable.InitHandler)
 	config.RegistFilterHandler(filterkv.ModuleName, filterkv.InitHandler)
+	config.RegistFilterHandler(kv.ModuleName, kv.InitHandler)
 
 	config.RegistOutputHandler(outputamqp.ModuleName, outputamqp.InitHandler)
 	config.RegistOutputHandler(outputcond.ModuleName, outputcond.InitHandler)
