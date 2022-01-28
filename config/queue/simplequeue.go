3fc5669a2cf388de888f07859bfed82fc7083882
helgeolav <39164025+helgeolav@users.noreply.github.com>
2022-01-28 01:04:00 +0100

    Added backpressure to outputnsq (#182)
    * Added backpressure to outputnsq
    
    Co-authored-by: Helge Olav Helgesen <1-bitbucket@helge.net>

--- config/queue/simplequeue.go
+++ config/queue/simplequeue.go
@@ -15,19 +15,19 @@ type simpleQueue struct {
 	RetryInterval uint `json:"retry_interval" yaml:"retry_interval"` // seconds before a new retry in case on error
 	MaxQueueSize  int  `json:"max_queue_size" yaml:"max_queue_size"` // max size of queue before deleting events (-1=no limit, 0=disable)
 
-	ctx        context.Context
-	output     QueueReceiver // the output
-	control    config.Control
-	isInPause  uint32                 // set to either StatusDelivering or StatusPaused
-	queue      chan logevent.LogEvent // channel to send events to the internal queue
-	retryqueue list.List              // list of queued messages; the list is not multithreading safe, and must only be accessed from backgroundtask()
+	ctx       context.Context
+	output    QueueReceiver // the output
+	control   config.Control
+	isInPause uint32           // set to either StatusDelivering or StatusPaused
+	queue     chan interface{} // channel to send events to the internal queue
+	codecCh   chan []byte      // channel to push a coded message onto
 }
 
 // Resume informs that the output is working again - can be called multiple times and is thread safe.
 // Should be called after each successfully delivery by the output.
 func (t *simpleQueue) Resume(ctx context.Context) error {
 	if atomic.CompareAndSwapUint32(&t.isInPause, StatusPaused, StatusDelivering) {
-		goglog.Logger.Debug("queue: requesting resume")
+		goglog.Logger.Debugf("queue %s: requesting resume", t.GetType())
 		return t.control.RequestResume(ctx)
 	}
 	return nil
@@ -35,12 +35,12 @@ func (t *simpleQueue) Resume(ctx context.Context) error {
 
 // Queue queues an event into the queue, blocking if necessary until cancelled. Queue is used from the output to put something into the queue.
 // A call to add an event onto the queue will also pause the input.
-func (t *simpleQueue) Queue(ctx context.Context, event logevent.LogEvent) error {
+func (t *simpleQueue) Queue(ctx context.Context, event interface{}) error {
 	if atomic.CompareAndSwapUint32(&t.isInPause, StatusDelivering, StatusPaused) {
-		goglog.Logger.Debug("queue is requesting pause")
+		goglog.Logger.Debugf("queue %s is requesting pause", t.GetType())
 		err := t.control.RequestPause(ctx)
 		if err != nil {
-			goglog.Logger.Error("queue: ", err.Error())
+			goglog.Logger.Errorf("queue %s: %s", t.GetType(), err.Error())
 		}
 	}
 	select {
@@ -52,7 +52,7 @@ func (t *simpleQueue) Queue(ctx context.Context, event logevent.LogEvent) error 
 }
 
 // NewSimpleQueue returns a new queue using a simple retry/queuing mechanism. receiver is your Output object. queueSize is the number of events that will be queued before dropping events from the queue; with a value of -1 the sky is the limit. retryInterval is the time in seconds between each retry.
-func NewSimpleQueue(ctx context.Context, control config.Control, receiver QueueReceiver, queueSize int, retryInterval uint) Queue {
+func NewSimpleQueue(ctx context.Context, control config.Control, receiver QueueReceiver, outch chan []byte, queueSize int, retryInterval uint) Queue {
 	var chanSize int
 	if queueSize < 1 {
 		chanSize = 1
@@ -66,8 +66,8 @@ func NewSimpleQueue(ctx context.Context, control config.Control, receiver QueueR
 		output:        receiver,
 		control:       control,
 		isInPause:     StatusDelivering,
-		queue:         make(chan logevent.LogEvent, chanSize),
-		retryqueue:    list.List{},
+		queue:         make(chan interface{}, chanSize),
+		codecCh:       outch,
 	}
 	go s.backgroundtask()
 	return s
@@ -97,56 +97,84 @@ func (t *simpleQueue) Output(ctx context.Context, event logevent.LogEvent) (err 
 
 // backgroundtask is running in the background and adds new events to the queue, tries to send them out on the RetryInterval.
 func (t *simpleQueue) backgroundtask() {
-	goglog.Logger.Debug("backgroundtask started")
+	var retryqueue list.List // list of queued messages; the list is not multithreading safe, and must only be accessed from backgroundtask()
+
+	goglog.Logger.Debug("backgroundtask started for ", t.GetType())
 	dur := time.Duration(t.RetryInterval) * time.Second
 	ticker := time.NewTicker(dur)
 	defer ticker.Stop()
 	for {
 		select {
 		case event := <-t.queue:
-			if (t.retryqueue.Len() < t.MaxQueueSize) || t.MaxQueueSize == -1 {
-				t.retryqueue.PushBack(event)
+			if (retryqueue.Len() < t.MaxQueueSize) || t.MaxQueueSize == -1 {
+				retryqueue.PushBack(event)
 			}
 		case <-t.ctx.Done():
-			goglog.Logger.Debug("queue closing")
+			goglog.Logger.Debugf("queue %s closing", t.GetType())
 			return
 		case <-ticker.C:
 			// We have reached a RetryInterval. If there are any events in the queue, lets send one back.
 			// If we are still in pause mode we will send one, if we are in normal mode we will send all back.
-			if e := t.retryqueue.Front(); e != nil {
+			if e := retryqueue.Front(); e != nil {
+				goglog.Logger.Debugf("%s simplequeue trying to deliver, has %v events in queue", t.GetType(), retryqueue.Len())
 				if atomic.LoadUint32(&t.isInPause) == StatusPaused {
-					event := e.Value.(logevent.LogEvent)
-					t.retryqueue.Remove(e)
-					go func() {
-						ctx, cancel := context.WithTimeout(context.Background(), dur)
-						err := t.output.OutputEvent(ctx, event)
-						if err != nil {
-							goglog.Logger.Error("queue: sendone ", err.Error())
-						}
-						cancel()
-					}()
+					msg := e.Value
+					retryqueue.Remove(e)
+					switch v := msg.(type) {
+					case logevent.LogEvent:
+						go func() {
+							ctx, cancel := context.WithTimeout(context.Background(), dur)
+							err := t.output.OutputEvent(ctx, v)
+							if err != nil {
+								goglog.Logger.Errorf("queue: %ssendone %s", t.GetType(), err.Error())
+							}
+							cancel()
+						}()
+					case []byte:
+						go func() {
+							select {
+							case <-t.ctx.Done():
+								goglog.Logger.Debugf("queue %s closing", t.GetType())
+								return
+							case t.codecCh <- v:
+							}
+						}()
+					default:
+						goglog.Logger.Errorf("Invalid type %T", v)
+					}
 				} else {
 					// we are not in pause mode and will queue all the events in the queue for sending.
 					// First we need to empty the queue and get all events to send.
-					myList := []*logevent.LogEvent{}
+					myList := []interface{}{}
 					for {
-						e := t.retryqueue.Front()
+						e := retryqueue.Front()
 						if e == nil {
 							break
 						}
-						event := e.Value.(logevent.LogEvent)
-						myList = append(myList, &event)
-						t.retryqueue.Remove(e)
+						event := e.Value
+						myList = append(myList, event)
+						retryqueue.Remove(e)
 					}
 					// Now we have to send them all out
 					go func() {
 						for x := range myList {
 							ctx, cancel := context.WithTimeout(context.Background(), dur)
-							err := t.Output(ctx, *myList[x])
-							if err != nil {
-								goglog.Logger.Error("queue: sendall", err.Error())
+							defer cancel()
+							switch v := (myList[x]).(type) {
+							case []byte:
+								select {
+								case <-t.ctx.Done():
+									return
+								case t.codecCh <- v:
+								}
+							case logevent.LogEvent:
+								err := t.Output(ctx, v)
+								if err != nil {
+									goglog.Logger.Errorf("queue: %s sendall %s", t.GetType(), err.Error())
+								}
+							default:
+								goglog.Logger.Errorf("Invalid type %T", v)
 							}
-							cancel()
 						}
 					}()
 				}
---
+++ config/queue/simplequeue_codec_test.go
@@ -0,0 +1,106 @@
+package queue
+
+import (
+	"context"
+	"github.com/tsaikd/gogstash/config/logevent"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+// This test will check that we receive messages after a codec ([]byte) has processed the stream.
+// At this point we no longer have access to logevent.Logevent, only the coded []byte stream.
+
+type codecOutput struct {
+	numReceived uint32          // increments for each message received (as []byte) on channel
+	numSuccess  uint32          // successful deliveries
+	numIncoming uint32          // increments for each message delivered to OutputEvent()
+	numQueued   uint32          // number of messages sent to queue
+	queue       Queue           // our queue
+	ch          chan []byte     // the channel we will receive on
+	ctx         context.Context // to listen for stop signal
+	FailMsgId   []uint32        // received message # to fail on
+}
+
+// background runs in the background, counting number of successfully received messages
+func (c *codecOutput) background() {
+	for {
+		select {
+		case <-c.ctx.Done():
+			return
+		case msg := <-c.ch:
+			id := atomic.AddUint32(&c.numReceived, 1)
+			var failed bool
+			for x := range c.FailMsgId {
+				if c.FailMsgId[x] == id {
+					failed = true
+					break
+				}
+			}
+			if failed {
+				err := c.queue.Queue(c.ctx, msg)
+				if err != nil {
+					panic(err)
+				}
+				atomic.AddUint32(&c.numQueued, 1)
+			} else {
+				atomic.AddUint32(&c.numSuccess, 1)
+			}
+		}
+	}
+}
+
+func (c *codecOutput) GetType() string {
+	return "codecOutput"
+}
+
+func (c *codecOutput) OutputEvent(ctx context.Context, event logevent.LogEvent) error {
+	atomic.AddUint32(&c.numIncoming, 1)
+	msg, err := event.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	c.ch <- msg
+	return nil
+}
+
+func TestSimpleQueueCodec(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	o := &codecOutput{
+		queue:     nil,
+		ch:        make(chan []byte, 10),
+		ctx:       ctx,
+		FailMsgId: []uint32{2},
+	}
+	control := newControlCounter()
+	q := NewSimpleQueue(ctx, control, o, o.ch, 5, 1)
+	o.queue = q
+	// now send a message that should go through
+	event := logevent.LogEvent{Timestamp: time.Now()}
+	go o.background()
+	err := q.Output(ctx, event)
+	if err == nil {
+		time.Sleep(time.Second)
+		if o.numSuccess != 1 {
+			t.Error("Sent one messages, did not get through")
+		}
+	} else {
+		t.Error(err)
+	}
+	// now send a message that should be queued
+	err = q.Output(ctx, event)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	time.Sleep(300 * time.Millisecond)
+	if o.numQueued != 1 || o.numReceived != 2 || o.numSuccess != 1 {
+		t.Errorf("Queueing does not work, %v", *o)
+		return
+	}
+	time.Sleep(900 * time.Millisecond)
+	if o.numSuccess != 2 {
+		t.Error("Two messages not delivered")
+	}
+}
